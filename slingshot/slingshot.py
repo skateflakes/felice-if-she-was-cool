68db2cf30817f2058c84dfca7010a4f526d08173
Marblyn ♍ <56658781+marblyn@users.noreply.github.com>
2025-03-04 22:42:26 -0600

    Update slingshot.py

--- slingshot/slingshot.py
+++ slingshot/slingshot.py
@@ -12,7 +12,7 @@ class Screenshot(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.width = 525
-        self.height = 950
+        self.height = 1000
         self.url = "https://htfslingshot.endersfund.com/htf_slingshot/web/sidebar/weekly"
 
     async def take_screenshot(self, path: str):
