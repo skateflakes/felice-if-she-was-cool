41a3347fb88a2ecc2c2771ad6909b8a843f55ebf
Marblyn ♍ <56658781+marblyn@users.noreply.github.com>
2025-03-04 22:17:20 -0600

    Update __init__.py

--- slingshot/__init__.py
+++ slingshot/__init__.py
@@ -1,5 +1,5 @@
 from redbot.core.bot import Red
 from .slingshot import Screenshot
 
-def setup(bot: Red):
-    bot.add_cog(Screenshot(bot))
+async def setup(bot: Red):
+    await bot.add_cog(Screenshot(bot))
