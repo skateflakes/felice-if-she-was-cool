cd87918fb6dc270693802fb82752c897af061181
Marblyn ♍ <56658781+marblyn@users.noreply.github.com>
2025-03-06 15:40:19 -0600

    Update slingshot.py

--- slingshot/slingshot.py
+++ slingshot/slingshot.py
@@ -17,9 +17,8 @@ class Screenshot(commands.Cog):
     async def take_screenshot(self, path: str):
         options = Options()
         options.add_argument("--headless")
-        options.add_argument(f"--window-size={self.width},{self.height}")
-        
-                driver = webdriver.Chrome(options=options)
+        options.add_argument(f"--window-size={self.width},{self.height}")        
+        driver = webdriver.Chrome(options=options)
         driver.get(self.url)
         driver.execute_script("document.body.style.zoom='175%'")  # Zoom in by 175%
         driver.set_window_size(self.width, self.height)  # Resize the page
