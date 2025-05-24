8b78418ec680f695749df6920d5dbd84ed031faf
Marblyn ♍ <56658781+marblyn@users.noreply.github.com>
2025-05-24 15:23:18 -0500

    Update __init__.py

--- reddit/__init__.py
+++ reddit/__init__.py
@@ -1,4 +1,6 @@
 from .core import RedditPostListener
+from .moderation import RedditModeration
 
 async def setup(bot):
     await bot.add_cog(RedditPostListener(bot))
+    await bot.add_cog(RedditModeration(bot))
