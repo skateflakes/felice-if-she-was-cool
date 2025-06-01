d51fb74652d2ca5dafcd3545d08abb9accc0cf58
Marblyn ♍ <56658781+marblyn@users.noreply.github.com>
2025-06-01 16:22:43 -0500

    Create marblyn.py

---
+++ other/marblyn.py
@@ -0,0 +1,23 @@
+from redbot.core import commands
+import discord
+
+class Marblyn(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if message.author.bot:
+            return
+
+        if "marblyn" in message.content.lower():
+            user_id = 257596407911940097
+            user = self.bot.get_user(user_id) or await self.bot.fetch_user(user_id)
+
+            if user:
+                try:
+                    await user.send(
+                        f'"marblyn" was mentioned in {message.channel.mention} by {message.author.mention}:\n> {message.content}'
+                    )
+                except discord.Forbidden:
+                    pass  # Cannot DM user
