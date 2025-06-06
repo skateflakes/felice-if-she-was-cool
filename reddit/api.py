2f0f4289434538217f93f12feea753483ec3e380
Marblyn ♍ <56658781+marblyn@users.noreply.github.com>
2025-06-05 19:31:21 -0500

    Update api.py

--- reddit/api.py
+++ reddit/api.py
@@ -1,11 +1,12 @@
 import asyncpraw
-from redbot.core import commands
 
 async def get_reddit_instance(bot):
-    client_id = await bot.get_shared_api_tokens("reddit").get("client_id")
-    client_secret = await bot.get_shared_api_tokens("reddit").get("client_secret")
-    username = await bot.get_shared_api_tokens("reddit").get("username")
-    password = await bot.get_shared_api_tokens("reddit").get("password")
+    tokens = await bot.get_shared_api_tokens("reddit")
+
+    client_id = tokens.get("client_id")
+    client_secret = tokens.get("client_secret")
+    username = tokens.get("username")
+    password = tokens.get("password")
 
     if not all([client_id, client_secret, username, password]):
         raise ValueError("Missing Reddit API credentials. Use [p]set api reddit ...")
@@ -15,6 +16,6 @@ async def get_reddit_instance(bot):
         client_secret=client_secret,
         username=username,
         password=password,
-        user_agent="HTFBot Discord moderation cog (by u/{})".format(username)
+        user_agent=f"HTFBot Discord moderation cog (by u/{username})"
     )
     return reddit
